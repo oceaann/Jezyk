2e28e4dded75281bb08d3c9a9a3a2fe49a0c2581
ocean <47253870+oceaann@users.noreply.github.com>
2022-08-20 21:59:26 +0200

    userToEmbedAuthor method

--- src/lib/structures/Embed.js
+++ src/lib/structures/Embed.js
@@ -1,14 +1,1 @@
-const { EmbedBuilder } = require('discord.js');
-
-module.exports = class Embed extends EmbedBuilder {
-    constructor(user) {
-        super();
-
-        this.user = user;
-
-        this.setAuthor({
-            name: this.user.tag,
-            iconURL: this.user.displayAvatarURL(),
-        });
-    }
-};
+export const userToEmbedAuthor = (user) => ({ name: `${user.username}#${user.discriminator}`, icon_url: user.avatarURL })
