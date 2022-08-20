2c78f11f55a91e5a11e6dcd6c6701fdfb3f0dd12
ocean <47253870+oceaann@users.noreply.github.com>
2022-08-20 11:58:00 +0200

    delete unused file

--- src/events/guild/messageCreate.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'messageCreate',
-    run: async (client, message) => {
-        // console.log(`${message.guild.name}] - ${message.author.tag}: ${message.content}`);
-    },
-};
