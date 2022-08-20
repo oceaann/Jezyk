f1647fe8e2124fd0f69c088c424d6e63ab26ad61
ocean <47253870+oceaann@users.noreply.github.com>
2022-08-20 21:56:30 +0200

    new options handling system

--- src/events/guild/interactionCreate.js
+++ src/events/guild/interactionCreate.js
@@ -1,11 +1,28 @@
-module.exports = {
-    name: 'interactionCreate',
-    run: async (client, interaction) => {
-        if (interaction.isChatInputCommand()) {
-            const command = client.commands.get(interaction.commandName);
-            if (!command) return interaction.reply('nie działam bo sie zepsułem a discord zepsuł jeszcze bardziej');
+import { client, commands } from "../../index.js";
+import { Constants } from "eris";
 
-            command.execute({ client, interaction });
-        }
-    },
-};
+const name = 'interactionCreate'
+
+const run = async (interaction) => {
+    if (interaction.type === 2) {
+        await interaction.defer()
+
+        const command = commands.get(interaction.data.name);
+        if (!command) return interaction.createFollowup('nie działam bo sie zepsułem a discord zepsuł jeszcze bardziej');
+
+        const options = new Map()
+        addOptions(interaction.data.options, options);
+
+        command.execute(interaction, options);
+    }
+}
+
+const addOptions = (data = [], map) => {
+    for (const option of data) {
+        if(data.type < Constants.ApplicationCommandOptionTypes.STRING) {
+            addOptions(option.options, map)
+        } else { map.set(option.name, option.value) }
+    }
+}
+
+export { name, run };
